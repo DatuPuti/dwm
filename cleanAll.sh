1a9472771229da491e411a85a7dd67a0aa972a33
DatuPuti <datuputi1@gmail.com>
2026-06-13 15:39:41 -0400

    added commandline argument to delete exe's

--- cleanAll.sh
+++ cleanAll.sh
@@ -1,19 +1,38 @@
 #!/usr/bin/zsh
 
+DELETE_BIN=false
 
 cd dmenu
 make clean
-sudo rm -rf /usr/local/bin/dmenu
 
 cd ../dwm-6.8
 make clean
-sudo rm -rf /usr/local/bin/dwm
 
 cd ../slstatus-1.1
 make clean
-sudo rm -rf /usr/local/bin/slstatus
 
 cd ../st-0.9.3
 make clean
-sudo rm -rf /usr/local/bin/st
 
+cd ..
+case "$1" in
+    --with-bin)
+        sudo rm -rf /usr/local/bin/st
+        sudo rm -rf /usr/local/bin/dwm
+        sudo rm -rf /usr/local/bin/dmenu
+        sudo rm -rf /usr/local/bin/slstatus
+        ;;
+    --help|-h)
+        echo "Usage: $0 [--with-bin]"
+        echo "  --with-bin   Also delete installed executable"
+        exit 0
+        ;;
+    "")
+        # no args: normal cleanup including exe's
+        ;;
+        *)
+        echo "Unknown option: $1"
+        echo "Use --help for usage"
+        exit 1
+        ;;
+esac
